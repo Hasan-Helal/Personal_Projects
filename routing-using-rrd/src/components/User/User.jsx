6ab5ec9f3bb5a577c8fc0dce37ded70054941d87
Hasan Helal <138429561+Hasan-Helal@users.noreply.github.com>
2023-12-22 17:27:39 +0530

    my second react project

---
+++ routing-using-rrd/src/components/User/User.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+// const {userid} = useParams()
+
+function User() {
+  return <div>User: {userid}</div>;
+}
+
+export default User;
